c66e8684b9b32308065a3b4238c44b69d0b8b186
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-08 00:11:48 -0500

    Update rutas.h

--- rutas.h
+++ rutas.h
@@ -1,11 +1,11 @@
-#include<iostream>
-#include<string>
-using namespace std;
+#ifndef RUTAS_H_INCLUDED
+#define RUTAS_H_INCLUDED
+#include"funciones.h"
 
 void verificar_string(string &frase, int clave)
 {
     int a=frase.size();
-    if(a%clave !=0)
+    if(mod(a,clave) !=0)
     {
         int res=a/clave;
         res++;
@@ -14,6 +14,7 @@ void verificar_string(string &frase, int clave)
             frase+="w";
     }
 }
+
 void sub1(string msj1, string &msj2,int &entrada,int &salida, int n1, int k, int c, int modo)
 {
     if (modo==1)
@@ -35,6 +36,7 @@ void sub1(string msj1, string &msj2,int &entrada,int &salida, int n1, int k, int
         }
     }
 }
+
 void sub2(string msj1, string &msj2,int &entrada,int &salida, int n2, int k,int modo)
 {
     if(modo==1)
@@ -56,6 +58,7 @@ void sub2(string msj1, string &msj2,int &entrada,int &salida, int n2, int k,int 
         }
     }
 }
+
 void sub3(string msj1, string &msj2,int &entrada,int &salida, int n1, int k, int c,int modo)
 {
     if(modo==1)
@@ -77,6 +80,7 @@ void sub3(string msj1, string &msj2,int &entrada,int &salida, int n1, int k, int
         }
     }
 }
+
 void sub4(string msj1, string &msj2,int &entrada,int &salida, int n2, int k, int modo)
 {
     if(modo==1)
@@ -107,127 +111,59 @@ public:
     {
         clave=c;
     }
-    string cifrar(string msj1)
+    string op(string msj1, int modo)
     {
+        if (modo==1)
+            verificar_string(msj1,clave);
+
         int n1=1;
         int n2=1;
-        int n3=0;
         string msj2;
-        verificar_string(msj1,clave);
+
         msj2=msj1;
-        int x=0;
         int entrada=0;
         int salida=0;
         int c=msj1.size()/clave;
+        
+        if (c==1)
+            return msj1;
+        
         bool g = true;
 
         msj2[0]=msj1[0];
-        sub1(msj1,msj2,entrada,salida,n1,clave,c,1);
-        sub2(msj1,msj2,entrada,salida,n2,clave,1);
-        sub3(msj1,msj2,entrada,salida,n1,clave,c,1);
+        sub1(msj1,msj2,entrada,salida,n1,clave,c,modo);
+        sub2(msj1,msj2,entrada,salida,n2,clave,modo);
+        sub3(msj1,msj2,entrada,salida,n1,clave,c,modo);
         n2++;
         n1++;
 
-        while(true)
+        while(g==true)
         {
-            if (x==0)
-            {
-                g = (clave-n2)>0;
-                if(g==false){return msj2;}
-                sub4(msj1,msj2,entrada,salida,n2,clave,1);
-                n2++;
-                n3=0;
-                x=1;
-            }
-            if (x==1)
-            {
-                g = (c-n1)>0;
-                if(g==false){return msj2;}
-                sub1(msj1,msj2,entrada,salida,n1,clave,c,1);
-                n1++;
-                n3=1;
-                x=2;
-            }
-            if (x==2)
-            {
-                g = (clave-n2)>0;
-                if(g==false){return msj2;}
-                sub2(msj1,msj2,entrada,salida,n2,clave,1);
-                n2++;
-                n3=2;
-                x=3;
-            }
-            if (x==3)
-            {
-                g = (c-n1)>0;
-                if(g==false){return msj2;}
-                sub3(msj1,msj2,entrada,salida,n1,clave,c,1);
-                n1++;
-                n3=3;
-                x=0;
-            }
+            g = (clave-n2)>0;
+            sub4(msj1,msj2,entrada,salida,n2,clave,modo);
+            n2++;
+
+            g = (c-n1)>0;
+            sub1(msj1,msj2,entrada,salida,n1,clave,c,modo);
+            n1++;
+
+            g = (clave-n2)>0;
+            sub2(msj1,msj2,entrada,salida,n2,clave,modo);
+            n2++;
+
+            g = (c-n1)>0;
+            sub3(msj1,msj2,entrada,salida,n1,clave,c,modo);
+            n1++;
         }
         return msj2;
     }
-
-    string d_1(string msj1)
+    string cifrar(string &m)
     {
-        int n1=1;
-        int n2=1;
-        string msj2;
-        msj2=msj1;
-        int x=0;
-        int entrada=0;
-        int salida=0;
-        int c=msj1.size()/clave;
-        bool g = true;
-
-        msj2[0]=msj1[0];
-        sub1(msj1,msj2,entrada,salida,n1,clave,c,0);
-        sub2(msj1,msj2,entrada,salida,n2,clave,0);
-        sub3(msj1,msj2,entrada,salida,n1,clave,c,0);
-        n2++;
-        n1++;
-            while(true)
-        {
-            if (x==0)
-            {
-                g = (clave-n2)>0;
-                if(g==false){return msj2;}
-                sub4(msj1,msj2,entrada,salida,n2,clave,0);
-                n2++;
-                x=1;
-            }
-            if (x==1)
-            {
-            g = (c-n1)>0;
-                if(g==false){return msj2;}
-                sub1(msj1,msj2,entrada,salida,n1,clave,c,0);
-                n1++;
-                x=2;
-            }
-            if (x==2)
-            {
-                g = (clave-n2)>0;
-                if(g==false){return msj2;}
-                sub2(msj1,msj2,entrada,salida,n2,clave,0);
-                n2++;
-                x=3;
-            }
-            if (x==3)
-            {
-                g = (c-n1)>0;
-                if(g==false){return msj2;}
-                sub3(msj1,msj2,entrada,salida,n1,clave,c,0);
-                n1++;
-                x=0;
-            }
-        }
-        return msj2;
+        return op(m,1);
     }
     string descifrar(string m)
     {
-        string n = d_1(m);
+        string n = op(m,0);
         int x=n.size()-1;
         while(n[x]=='w')
         {
@@ -237,3 +173,4 @@ public:
         return n;
     }
 };
+#endif // RUTAS_H_INCLUDED
