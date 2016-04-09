96518750ba81be8496ed2aa86864e0d6ef4f1918
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-08 19:33:33 -0500

    Update rutas2.cpp

--- rutas2.cpp
+++ rutas2.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     rutas a(4);
-    string x="holaquetalctmreabeldemrdoiasdjas";
+    string x="holaquetaldoiasdjas";
     string g= a.cifrar(x);
     cout<<g<<endl;
     string y=a.descifrar(g);
