b4485bb6bd7fed92b40c436c42f8e42932c449c7
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-10 13:58:16 -0500

    Update rutas.h

--- vallas-rutas/rutas.h
+++ vallas-rutas/rutas.h
@@ -2,18 +2,6 @@
 #define RUTAS_H_INCLUDED
 #include"funciones.h"
 
-void verificar_string(string &frase, int clave)
-{
-    int a=frase.size();
-    if(mod(a,clave) !=0)
-    {
-        int res=a/clave;
-        res++;
-        int b=res*clave;
-        for(int i=0;i<(b-a);i++)
-            frase+="w";
-    }
-}
 
 void sub1(string msj1, string &msj2,int &entrada,int &salida, int n1, int k, int c, int modo)
 {
@@ -113,9 +101,6 @@ public:
     }
     string op(string msj1, int modo)
     {
-        if (modo==1)
-            verificar_string(msj1,clave);
-
         int n1=1;
         int n2=1;
         string msj2;
@@ -128,8 +113,6 @@ public:
         if (c==1)
             return msj1;
 
-        bool g = true;
-
         msj2[0]=msj1[0];
         sub1(msj1,msj2,entrada,salida,n1,clave,c,modo);
         sub2(msj1,msj2,entrada,salida,n2,clave,modo);
@@ -137,29 +120,44 @@ public:
         n2++;
         n1++;
 
+        bool g = clave-n2>0;
         while(g==true)
         {
             g = (clave-n2)>0;
+            if (g==false){return msj2;}
             sub4(msj1,msj2,entrada,salida,n2,clave,modo);
             n2++;
 
             g = (c-n1)>0;
+            if (g==false){return msj2;}
             sub1(msj1,msj2,entrada,salida,n1,clave,c,modo);
             n1++;
 
             g = (clave-n2)>0;
+            if (g==false){return msj2;}
             sub2(msj1,msj2,entrada,salida,n2,clave,modo);
             n2++;
 
             g = (c-n1)>0;
+            if (g==false){return msj2;}
             sub3(msj1,msj2,entrada,salida,n1,clave,c,modo);
             n1++;
         }
         return msj2;
     }
-    string cifrar(string &m)
+    string cifrar(string m)
     {
-        return op(m,1);
+        string n = m;
+        int a=m.size();
+        if(mod(a,clave) !=0)
+        {
+            int res=a/clave;
+            res++;
+            int b=res*clave;
+            for(int i=0;i<(b-a);i++)
+                n+="w";
+        }
+        return op(n,1);
     }
     string descifrar(string m)
     {
