35dc7cc01454d21dc5c2a07a6ce462998a2d059f
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-15 17:32:10 -0500

    Update funciones.h

--- vallas-rutas/funciones.h
+++ vallas-rutas/funciones.h
@@ -38,36 +38,45 @@ int mcd(int a, int b)
 
 void euclides_ext(int a, int b, int &fin1,int &fin2)
 {
-    int q,x,x_1,y,y_1,temp1,temp2,temp3;
+    int q,x,x_1,y,y_1,t;
+
+    if (a == 0)
+    {
+        fin1 = 1;
+        fin2 = 0;
+    }
 
     if (b > a)
     {
-        int t = a;
-        a=b;
-        b=t;
+        t = a;
+        a = b;
+        b = t;
     }
-    x=0;
-    y=1;
-    x_1=1;
-    y_1=0;
+
+    x = 0;
+    y = 1;
+    x_1 = 1;
+    y_1 = 0;
+
     while (b != 0)
     {
         q = a/b;
-        temp1 = a%b;
+        t = mod(a,b);
         a = b;
-        b = temp1;
+        b = t;
 
-        temp2 = x;
+
+        t = x;
         x = x_1 - q*x;
-        x_1 = temp2;
+        x_1 = t;
 
-        temp3 = y;
+        t = y;
         y = y_1 - q*y;
-        y_1 = temp3;
+        y_1 = t;
+
     }
     fin1=x_1;
     fin2=y_1;
 }
-
 
 #endif // FUNCIONES_H_INCLUDED
