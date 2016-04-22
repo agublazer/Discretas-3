631dbd5f1b3e5ca2d794bf39cd29929e096a8871
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-22 17:28:27 -0500

    Create cifrado_afin.h

---
+++ hola/cifrado_afin.h
@@ -0,0 +1,102 @@
+#ifndef CIFRADO_AFIN_H_INCLUDED
+#define CIFRADO_AFIN_H_INCLUDED
+
+#include<time.h>
+#include<stdlib.h>
+
+#include"funciones.h"
+
+class afin
+{
+private:
+    int clave_a;
+    int clave_b;
+public:
+    string alfabeto;
+    int tamano_alfabeto;
+    afin()
+    {
+        srand(time(NULL));
+        clave_b = rand()%100+1;
+        clave_a = 0;
+        alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        tamano_alfabeto = alfabeto.size();
+    }
+
+    afin(int a, int b)
+    {
+        clave_a=a;
+        clave_b=b;
+        alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        tamano_alfabeto = alfabeto.size();
+    }
+
+    void generar_clave()
+    {
+        cout<<clave_b<<endl;
+        srand(time(NULL));
+        clave_a = rand()%200+1;
+        int m = mcd(clave_a,clave_b);
+        while(m != 1)
+        {
+            //srand(time(NULL));
+            clave_a = rand()%200+1;
+            m = mcd(clave_a,clave_b);
+            cout<<clave_a<<endl;
+        }
+        cout<<"final: "<<clave_a<<endl;
+    }
+    string cifrar(string mensaje)
+    {
+        if (clave_a==0)
+            return "clave no generada";
+
+        string salida=mensaje;
+        int a;
+        int b;
+        int c = mensaje.size();
+        for(int i=0;i<c;i++)
+        {
+            a = alfabeto.find(mensaje[i]);
+            b = (clave_a*a) + clave_b;
+            if (a+b >= tamano_alfabeto)
+            {
+                salida[i]=alfabeto[mod(b,tamano_alfabeto)];
+            }
+            else
+            {
+                salida[i]=alfabeto[b];
+            }
+        }
+        return salida;
+    }
+    string descifrar(string mensaje)
+    {
+        string salida = mensaje;
+        int a,b,x,y;
+        euclides_ext(clave_a,tamano_alfabeto,x,y);
+        if (x<0)
+            x +=tamano_alfabeto;
+
+        int c = mensaje.size();
+        for(int i=0;i<c;i++)
+        {
+            a = alfabeto.find(mensaje[i]);
+            b = x*(a-clave_b);
+            if (b<0)
+            {
+                b = mod(b,tamano_alfabeto);
+                salida[i]=alfabeto[b];
+            }
+            else
+                salida[i]=alfabeto[b];
+
+
+        }
+        return salida;
+    }
+    int get_claveA(){return clave_a;}
+    int get_claveB(){return clave_b;}
+};
+
+#endif // CIFRADO_AFIN_H_INCLUDED
